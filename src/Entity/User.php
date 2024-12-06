454af3e5f4f37cfc9b06da2f2e3170ae2798bc4a
mohamed manseur <mohamedmanseur@MacBook-Pro-de-mohamed.local>
2024-12-06 15:07:52 +0100

    fix(user): email 500

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,6 +41,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ["email"], message: "email")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
